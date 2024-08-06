0c15619cb04c4ea3e34d6ff48003ac3b2de61f0b
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-08-06 21:07:25 +0530

    add struct and git igore file

--- Class-concepts/struct.c
+++ Class-concepts/struct.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include<string.h>
+
+struct employee 
+{
+    char name[30];
+    int id;
+    float salary;
+};
 
 int main()
 {
-    
+    struct employee e1;
+    strcpy(e1.name,"kush");
+    e1.id = 26;
+    e1.salary = 2000000;
+
+    printf("%s ",e1.name);
+    printf("%d ",e1.id);
+    printf("%f", e1.salary);
+    printf("\n");
+    return 0;
 }
