75a3e4cf9a4afa4ca3b51ed7726bff5c0b9f93d8
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-08-07 13:47:59 +0530

    3.c

--- lab3_07_08/3.c
+++ lab3_07_08/3.c
@@ -2,7 +2,47 @@
 
 int main()
 {
-    int r,c;
+    int r,c,k=0;
     printf("Enter the number of rows and columns : ");
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    printf("Enter the values of the sparse matrix");
+    for(int i =0;i<r;i++)
+        for(int j =0;j<c;j++)
+            scanf("%d",&a[i][j]);
     
+    int s[100][3];
+
+    for(int i =0;i<r;i++)
+    {
+        for(int j =0;j<c;j++)
+        {
+            if(a[i][j] != 0)
+            {
+                k++;
+                s[k][0] = i;
+                s[k][1] = j;
+                s[k][2] = a[i][j];
+            }
+        }
+    }
+
+    s[0][0] = r;
+    s[0][1] = c;
+    s[0][2] = k;
+
+    printf("%d",k);
+    printf("\nSparse Matrix : \n");
+
+    for(int i =0;i<=k;k++)
+    {
+        for(int j =0;j<3;j++)
+        {
+            printf("%d ",s[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
 }
