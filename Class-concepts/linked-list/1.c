06ff83ed6ffcf03b3985431fd5f7465a9ce6ec48
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-08-17 22:53:37 +0530

    added function to add a a node before a given node

--- Class-concepts/linked-list/1.c
+++ Class-concepts/linked-list/1.c
@@ -15,6 +15,8 @@ void addbegining(int n);
 
 void add_after_data(int given, int n);
 
+void add_before_data(int given, int n);
+
 int main()
 {
     int y = 1;
@@ -24,7 +26,8 @@ int main()
         printf("1. Add a node / Create a list\n");
         printf("2. Display all nodes\n");
         printf("3. Add node at start\n");
-        printf("4. Add a node after a given dat avalue\n");
+        printf("4. Add a node after a given data value\n");
+        printf("5. Add a node before a given data value\n");
         printf("0. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
@@ -36,20 +39,29 @@ int main()
             scanf("%d", &data);
             create_list(data);
             break;
+
         case 2:
             print_list();
             break;
+
         case 3:
             printf("Enter data to be entered : ");
             scanf("%d", &data);
             addbegining(data);
             break;
+
         case 4:
             printf("Enter the data to be entered and the data value after which the node has to be added: ");
             scanf("%d%d", &data, &given); 
             add_after_data(given, data);  
             break;
-
+        
+        case 5:
+            printf("Enter the data to be entered and the data value before which the node has to be added : ");
+            scanf("%d%d",&data, &given);
+            add_before_data(given,data);
+            break;
+            
         case 0:
             break;
         default:
@@ -138,4 +150,29 @@ void add_after_data(int given, int n)
         printf("Data value %d not found in the list.\n", given);
         free(ptr);
     }
+}
+
+void add_before_data(int given, int n)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = n;
+    ptr->next = NULL;
+
+    if (start == NULL)
+    {
+        printf("List is empty.\n");
+        free(ptr);
+        return;
+    }
+
+    struct node* temp = start;
+    struct node* q = start;
+
+    while(temp->data != given && temp != NULL)
+        {
+            q = temp;
+            temp = temp->next;
+        }
+        q->next = ptr;
+        ptr->next = temp;
 }
