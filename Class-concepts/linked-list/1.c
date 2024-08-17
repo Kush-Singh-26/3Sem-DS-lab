7204430c8b46d65b6d313bc49efb9e92ecedf5d5
Kush-Singh-26 <kushsingh2004+webdev@gmail.com>
2024-08-17 22:33:11 +0530

    added function to adda a node after a given node

--- Class-concepts/linked-list/1.c
+++ Class-concepts/linked-list/1.c
@@ -8,25 +8,32 @@ struct node
 } *start = NULL;
 
 void create_list(int n);
+
 void print_list();
+
 void addbegining(int n);
+
+void add_after_data(int given, int n);
+
 int main()
 {
     int y = 1;
-    int data, choice;
-    do{
+    int data, choice, given;
+    do
+    {
         printf("1. Add a node / Create a list\n");
         printf("2. Display all nodes\n");
         printf("3. Add node at start\n");
+        printf("4. Add a node after a given dat avalue\n");
         printf("0. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d",&choice);
+        scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
             printf("Enter data to be entered : ");
-            scanf("%d",&data);
+            scanf("%d", &data);
             create_list(data);
             break;
         case 2:
@@ -34,19 +41,23 @@ int main()
             break;
         case 3:
             printf("Enter data to be entered : ");
-            scanf("%d",&data);
+            scanf("%d", &data);
             addbegining(data);
             break;
+        case 4:
+            printf("Enter the data to be entered and the data value after which the node has to be added: ");
+            scanf("%d%d", &data, &given); 
+            add_after_data(given, data);  
+            break;
+
         case 0:
             break;
         default:
             printf("Incorrect choice\n");
             break;
         }
-        
-        
-    } while(choice != 0);
-    
+
+    } while (choice != 0);
 }
 
 void create_list(int n)
@@ -77,11 +88,11 @@ void print_list()
 
     else
     {
-        struct node* temp = start;
+        struct node *temp = start;
 
-        while(temp != NULL)
+        while (temp != NULL)
         {
-            printf("%d ",temp->data);
+            printf("%d ", temp->data);
             temp = temp->next;
         }
 
@@ -91,8 +102,40 @@ void print_list()
 
 void addbegining(int n)
 {
-    struct node* ptr = (struct node*) malloc(sizeof(struct node));
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
     ptr->data = n;
     ptr->next = start;
     start = ptr;
+}
+
+void add_after_data(int given, int n)
+{
+    struct node *ptr = (struct node *)malloc(sizeof(struct node));
+    ptr->data = n;
+    ptr->next = NULL;
+
+    if (start == NULL)
+    {
+        printf("List is empty.\n");
+        free(ptr);
+        return;
+    }
+
+    struct node *temp = start;
+
+    while (temp != NULL && temp->data != given)
+    {
+        temp = temp->next;
+    }
+
+    if (temp != NULL)
+    {
+        ptr->next = temp->next;
+        temp->next = ptr;
+    }
+    else
+    {
+        printf("Data value %d not found in the list.\n", given);
+        free(ptr);
+    }
 }
